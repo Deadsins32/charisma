10809e260f5434b2b727ad7cb6968744630751a7
Red <30968528+shadeRed@users.noreply.github.com>
2019-02-03 20:42:48 -0600

    revamped set command

--- src/commands/management/set/set.js
+++ src/commands/management/set/set.js
@@ -5,179 +5,90 @@ module.exports = function(imports, parameters) {
     var embed = new Discord.RichEmbed();
     embed.setColor(imports.data.guilds[imports.guild.id].colors.accent);
 
+    function index(obj, is, value) {
+        try {
+            if (typeof is == 'string') { return index(obj,is.split('.'), value) }
+            else if (is.length == 1 && value !== undefined) { return obj[is[0]] = value }
+            else if (is.length == 0) { return obj }
+            else { return index(obj[is[0]], is.slice(1), value) }
+        }
+
+        catch(error) {}
+    }
+    
+    /*
+    > obj = {a:{b:{etc:5}}}
+
+    > index(obj,'a.b.etc')
+    5
+    > index(obj,['a','b','etc'])   #works with both strings and lists
+    5
+
+    > index(obj,'a.b.etc', 123)    #setter-mode - third argument (possibly poor form)
+    123
+
+    > index(obj,'a.b.etc')
+    123
+    */
+
     var keys = {
         charisma: {
-            status: function(parameters) {
-                if (imports.user.id == imports.config.master) {
-                    imports.client.user.setPresence({game: {name: parameters[1], type: 0}});
-                    embed.setDescription(`set status to "playing ${parameters[1]}"`);
-                }
-            }
+            status: 'string'
         },
 
         guild: {
             config: {
-                colors: {
-                    accent: function(parameters) {
-                        if (imports.Command.methods.color(parameters[1]).pass) {
-                            imports.data.guilds[imports.guild.id].colors.accent = parameters[1];
-                            embed.setColor(imports.data.guilds[imports.guild.id].colors.accent);
-                            embed.setDescription(`the accent color has been set to "${parameters[1]}"`);
-                        }
-
-                        else { embed.setDescription(`please enter a valid hexadecimal color`) }
-                    },
-
-                    logs: {
-                        joins: function(parameters) {
-                            if (imports.Command.methods.color(parameters[1]).pass) {
-                                imports.data.guilds[imports.guild.id].colors.logs.joins = parameters[1];
-                                embed.setColor(imports.data.guilds[imports.guild.id].colors.logs.joins);
-                                embed.setDescription(`the joins color has been set to "${parameters[1]}"`);
-                            }
-
-                            else { embed.setDescription(`please enter a valid hexadecimal color`) }
-                        },
-
-                        leaves: function(parameters) {
-                            if (imports.Command.methods.color(parameters[1]).pass) {
-                                imports.data.guilds[imports.guild.id].colors.logs.leaves = parameters[1];
-                                embed.setColor(imports.data.guilds[imports.guild.id].colors.logs.leaves);
-                                embed.setDescription(`the leaves color has been set to "${parameters[1]}"`);
-                            }
-
-                            else { embed.setDescription(`please enter a valid hexadecimal color`) }
-                        },
-
-                        nicknamechanges: function(parameters) {
-                            if (imports.Command.methods.color(parameters[1]).pass) {
-                                imports.data.guilds[imports.guild.id].colors.logs.nicknamechanges = parameters[1];
-                                embed.setColor(imports.data.guilds[imports.guild.id].colors.logs.nicknamechanges);
-                                embed.setDescription(`the nickname changes color has been set to "${parameters[1]}"`);
-                            }
-
-                            else { embed.setDescription(`please enter a valid hexadecimal color`) }
-                        },
-
-                        usernamechanges: function(parameters) {
-                            if (imports.Command.methods.color(parameters[1]).pass) {
-                                    imports.data.guilds[imports.guild.id].colors.logs.usernamechanges = parameters[1];
-                                    embed.setColor(imports.data.guilds[imports.guild.id].colors.logs.usernamechanges);
-                                    embed.setDescription(`the usernamechanges color has been set to "${parameters[1]}"`);
-                            }
-
-                            else { embed.setDescription(`please enter a valid hexadecimal color`) }
-                        }
-                    }
-                },
-
-                prefix: function(parameters) {
-                    if (imports.member.permissions.serialize().ADMINISTRATOR) {
-                        imports.data.guilds[imports.guild.id].config.prefix = parameters[1];
-                        embed.setDescription(`the prefix has been set to "${parameters[1]}"`);
-                    }
-
-                    else { embed.setDescription(`you don't have permission to do that`) }
-                },
-
-                description: function(parameters) {
-                    if (imports.member.permissions.serialize().ADMINISTRATOR) {
-                        imports.data.guilds[imports.guild.id].config.description = parameters[1];
-                        embed.setDescription(`the description has been set to "${parameters[1]}"`);
-                    }
-
-                    else { embed.setDescription(`you don't have permission to do that`) }
-                },
-
-                expcurve: function(parameters) {
-                    if (imports.member.permissions.serialize().ADMINISTRATOR) {
-                        if (imports.Command.methods.number(parameters[1]).pass) {
-                            var curve = parseFloat(parameters[1]);
-                            imports.data.guilds[imports.guild.id].config.expcurve = curve;
-                            embed.setDescription(`the expcurve has been set to ${parameters[1]}`);
-                        }
-
-                        else { embed.setDescription(`please specify a valid number`) }
-                    }
-
-                    else { embed.setDescription(`you don't have permission to do that`) }
-                },
-
-                logchannel: function(parameters) {
-                    if (imports.member.permissions.serialize().ADMINISTRATOR) {
-                        if (imports.Command.methods.channel(parameters[1]).pass) {
-                            var channel = imports.Command.methods.channel(parameters[1]).value;
-                            imports.data.guilds[imports.guild.id].config.logchannel = channel;
-                            embed.setDescription(`the logchannel has been set to "${imports.guild.channels.get(channel).name}"`);
-                        }
-
-                        else { embed.setDescription(`please specify a channel`) }
-                    }
-
-                    else { embed.setDescription(`you don't have permission to do that`) }
-                },
-
-                autorole: function(parameters) {
-                    if (imports.member.permissions.serialize().ADMINISTRATOR) {
-                        if (imports.Command.methods.role(parameters[1]).pass) {
-                            var role = imports.Command.methods.role(parameters[1]).value;
-                            imports.data.guilds[imports.guild.id].config.autorole = role;
-                            embed.setDescription(`the autorole has been set to @${imports.guild.roles.get(role).name}`);
-                        }
-
-                        else if (imports.guild.roles.find('name', parameters[1])) {
-                            var role = imports.guild.roles.find('name', parameters[1]);
-                            imports.data.guilds[imports.guild.id].config.autorole = role.id;
-                            embed.setDescription(`the autorole has been set to @${role.name}`);
-                        }
-
-                        else if (imports.guild.roles.get(parameters[1])) {
-                            var role = imports.guild.roles.get(parameters[1]);
-                            imports.data.guilds[imports.guild.id].config.autorole = role.id;
-                            embed.setDescription(`the autorole has been set to @${role.name}`);
-                        }
-
-                        else { embed.setDescription(`please specify a channel`) }
-                    }
-
-                    else { embed.setDescription(`you don't have permission to do that`) }
-                },
-
-                flavor: function(parameters) {
-                    if (imports.member.permissions.serialize().ADMINISTRATOR) {
-                        if (imports.Flavors.get(parameters[1]) != null) {
-                            imports.data.guilds[imports.guild.id].config.flavor = parameters[1];
-                            embed.setDescription(`the flavor has been set to "${parameters[1]}"`);
-                        }
-                    
-                        else { embed.setDescription(`the flavor "${parameters[1]}" does not exist`) }
-                    }
-
-                    else { embed.setDescription(`you don't have permission to do that`) }
-                }
-            }
-        },
-
-        profile: {
-            color: function(parameters) {
-                if (imports.Command.methods.color(parameters[1]).pass) {
-                    imports.data.users[imports.user.id].color = parameters[1];
-                    embed.setColor(parameters[1]);
-                    embed.setDescription(`the profile color has been set to ${parameters[1]}`);
-                }
-
-                else { embed.setDescription(`please enter a valid hexadecimal color`) }
+                prefix: 'string',
+                expcurve: 'number',
+                logchannel: 'channel',
+                autorole: 'role',
+                flavor: 'flavor'
             },
 
-            description: function(parameters) {
-                imports.data.users[imports.user.id].description = parameters[1];
-                embed.setDescription(`the profile description has been set to "${parameters[1]}"`);
+            colors: {
+                accent: 'color',
+                logs: {
+                    joins: 'color',
+                    leaves: 'color',
+                    nicknamechanges: 'color',
+                    usernamechanges: 'color'
+                }
             }
         }
     }
 
-    try { eval(`keys.${parameters[0]}`)(parameters) }
-    catch(error) { embed.setDescription(`\`${parameters[0]}\` does not exist`) }
+    var methods = {
+        string: function(input) { return input },
+        color: function(input) { if (imports.Command.methods.color(input).pass) { embed.setColor(input); return input } },
+        number: function(input) { if (!isNaN(input)) { return eval(input) } },
+        channel: function(input) { if (imports.Command.methods.channel(input).pass) { if (imports.guild.channels.get(imports.Command.methods.channel(input).value)) { return imports.Command.methods.channel(input).value } } },
+        role: function(input) { if (imports.guild.roles.find('name', input)) { return imports.guild.roles.find('name', input).id } },
+        flavor: function(input) { if (imports.Flavors.check(input)) { return input } }
+    }
+
+    var maps = {
+        guild: imports.data.guilds[imports.guild.id]
+    }
+
+    var overwrites = {
+        charisma: {
+            status: function(input) { imports.client.user.setPresence({ game: { name: input }, status: 'online' }) }
+        }
+    }
+
+    if (index(keys, parameters[0])) {
+        var type = index(keys, parameters[0]);
+        if (methods[type](parameters[1])) {
+            if (index(overwrites, parameters[0])) { index(overwrites, parameters[0])(methods[type](parameters[1])) }
+            else { index(maps, parameters[0], methods[type](parameters[1])) }
+            if (typeof methods[type](parameters[1]) == 'string') { parameters[1] = `"${parameters[1]}"` }
+            embed.setDescription(`\`${parameters[0]}\` has been set to ${parameters[1]}`);
+        }
+
+        else { embed.setDescription(`${parameters[1]} is not a \`${type}\``) }
+    }
+
+    else { embed.setDescription(`\`${parameters[0]}\` does not exist`) }
 
     imports.channel.send(embed);
 }
