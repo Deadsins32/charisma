1a90f845b00460828b1e2cf7f80fbf675c16c030
Red <30968528+shadeRed@users.noreply.github.com>
2019-03-01 22:42:56 -0600

    Update magicball.js

--- src/commands/fun/magicball/magicball.js
+++ src/commands/fun/magicball/magicball.js
@@ -1,11 +1,34 @@
-module.exports = function(imports, arguments) {
-    var textArray = [
-        'Certainly',
-        'Yes',
-        'No',
-        'Why would I tell you?',
-        'You don\'t want to know...'
+var Discord = require('discord.js');
+
+module.exports = function(imports, parameters) {
+    Math.seedrandom(parameters[0]);
+    var embed = new Discord.RichEmbed();
+    embed.setColor(imports.data.guilds[imports.guild.id].colors.accent);
+
+    var responses = [
+        `yup`,
+        `maybe`,
+        `obviously`,
+        `perhaps`,
+        `definitely`,
+        `probably`,
+        `definitely not`,
+        `nope`,
+        `nah`,
+        `no`,
+        `doubt it`,
+        `certainly`,
+        `yeah`,
+        `yes`,
+        `*it's a secret*`,
+        `I could tell you, but where's the fun in that :wink:`,
+        `why would I tell you?`,
+        `it's possible`,
+        `it's not unlikely`,
+        `the odds aren't too bad`,
+        `why are you asking me this?`
     ]
-    Math.seedrandom(arguments[0]);
-    imports.channel.send(textArray[Math.floor(Math.random() * textArray.length)]);
+
+    embed.setDescription(responses[Math.floor(Math.random() * responses.length)]);
+    imports.channel.send(embed);
 }
