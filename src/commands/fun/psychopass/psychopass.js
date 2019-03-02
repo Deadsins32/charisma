7235d7b1f0a1543ea3f19fb82a77820b286e62b0
Red <30968528+shadeRed@users.noreply.github.com>
2019-03-01 22:31:41 -0600

    Update psychopass.js

--- src/commands/fun/psychopass/psychopass.js
+++ src/commands/fun/psychopass/psychopass.js
@@ -21,7 +21,7 @@ module.exports = function(imports, parameters) {
         else if (member.id == imports.user.id) { isSelf = true }
 
         if (isSelf) {
-            name = 'Your '
+            name = 'your '
             moderate = '**you** are ' + moderate;
             severe = '**you** are ' + severe;
             lethal = '**you** pose a ' + lethal;
@@ -59,67 +59,7 @@ module.exports = function(imports, parameters) {
     var embed = new Discord.RichEmbed();
     embed.setColor(imports.data.guilds[imports.guild.id].colors.accent);
 
-    if (title) { embed.setTitle(title) }
+    if (title) { description = `${title}\n${description}` }
     embed.setDescription(description);
-
     imports.channel.send(embed);
-
-    /*if (parameters[0]) {
-        if (parameters[0] == imports.client.user.id) {
-            isSelf = true;
-            identifier = imports.client.user.username;
-        }
-    }
-
-    if (arguments[0] == undefined) {
-        discriminator = imports.user.discriminator;
-        identifier = 'Your';
-        identifier2 = 'You are';
-        identifier3 = 'You pose';
-    }
-
-    else {
-        arguments[0] = imports.Command.methods.mention(arguments[0]).value;
-        if (arguments[0] == imports.client.user.id) {
-            isSelf = true;
-            identifier = imports.client.user.username;
-        }
-
-        else {
-            discriminator = imports.guild.members.get(arguments[0]).user.discriminator;
-            identifier = imports.guild.members.get(arguments[0]).displayName;
-            identifier2 = identifier + ' is';
-            identifier3 = identifier + ' poses';
-            if (identifier.slice(-1) == 's') {
-                identifier += "'";
-            }
-
-            else {
-                identifier += "'s";
-            }
-        }
-    }
-
-    message = identifier + ' Crime Coefficient is `' + coefficient + '`\n';
-
-    if (isSelf) {
-        message = identifier + "'s Crime Coefficient is `∞`\n" + identifier + ' is not a target for enforcement action. The trigger of the Dominator will now be thoroughly melted.';
-    }
-
-    else {
-        if (coefficient < 100) {
-            message += identifier2 + ' not a target for enforcement action. The trigger of the Dominator will be locked.';
-        }
-
-        else if (coefficient > 300) {
-            message += identifier3 + ' a serious threat to the society. Lethal force is authorized. The Dominator is set to Lethal Eliminator.';
-        }
-
-        else {
-            message += identifier2 + ' classified as a latent criminal and is a target for enforcement action. The Dominator will be set to Non-Lethal Paralyzer mode.';
-        }
-    }
-
-    embed.setDescription(message);
-    imports.channel.send(embed);*/
 }
