83bd92a04d090e24a330bb04f289dde788cb0baa
Thomas Achatz <red.shadium@gmail.com>
2019-03-12 14:58:01 -0500

    progress on exp curve calculator

--- docs/js/initializers/expChart.js
+++ docs/js/initializers/expChart.js
@@ -85,6 +85,72 @@ var layout = {
 };
 */
 
+/*
+data: [{
+        x: 10,
+        y: 20
+    }, {
+        x: 15,
+        y: 10
+    }]*/
+
+function expChart() {
+    var canvas = document.getElementById('expChart');
+
+    var data = new Array();
+    for (var l = 0; l <= 100; l++) {
+        data.push({x: l, y: levelToExp(l, 1.5)});
+    }
+
+    var options = {};
+
+    var chart = new Chart(canvas, {
+        type: 'line',
+        data: {
+            datasets: [data]
+        },
+        options: options
+    });
+
+    /*var myChart = new Chart(canvas, {
+        type: 'line',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });*/
+}
+
+/*
 function expChart() {
     var trace = {
         x: new Array(),
@@ -121,4 +187,4 @@ function expChart() {
     };
       
     Plotly.newPlot('expChart', data, layout);
-}
+}*/
