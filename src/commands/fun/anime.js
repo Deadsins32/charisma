7c57883ee9341b32b1722312c4a3aa578503801b
Thomas Achatz <30968528+shadeRed@users.noreply.github.com>
2019-06-10 14:02:12 -0500

    Update anime.js

--- src/commands/fun/anime.js
+++ src/commands/fun/anime.js
@@ -1,5 +1,5 @@
 var Discord = require('discord.js');
-var anime = require('node-kitsu');
+var kitsu = require('node-kitsu');
 
 module.exports = {
     config: {
@@ -13,27 +13,20 @@ module.exports = {
         ]
     },
 
-    command: function(imports, parameters) {
+    command: async function(imports, parameters) {
         var embed = new Discord.RichEmbed();
         embed.setColor(imports.local.guild.colors.accent);
-    
-        anime.searchAnime(parameters[0], 0).then(results => {
-            if (results[0]) {
-                embed.setTitle(results[0].attributes.canonicalTitle);
-                embed.setImage(results[0].attributes.coverImage.original);
-                embed.setFooter(imports.client.user.username, imports.client.user.avatarURL);
-    
-                embed.addField('status', results[0].attributes.status, true);
-                embed.addField('episodes', results[0].attributes.episodeCount, true);
-                embed.addField('episode length', results[0].attributes.episodeLength + 'm', true);
-                embed.addField('synopsis', results[0].attributes.synopsis.substring(0, 1021) + '...');
-                imports.channel.send(embed);
-            }
-    
-            else {
-                embed.setDescription(`"${parameters[0]}" was not found`);
-                imports.channel.send(embed);
-            }
-        });
+        var anime = await kitsu.searchAnime(parameters[0], 0);
+        if (anime && anime[0]) {
+            embed.setTitle(anime[0].attributes.canonicalTitle);
+            embed.setImage(anime[0].attributes.coverImage.original);
+            embed.addField('status', anime[0].attributes.status, true);
+            embed.addField('episodes', anime[0].attributes.episodeCount, true);
+            embed.addField('episode length', anime[0].attributes.episodeLength + 'm', true);
+            embed.addField('synopsis', anime[0].attributes.synopsis.substring(0, 1021) + '...');
+        }
+
+        else { embed.setDescription(`"${parameters[0]}" was not found`) }
+        imports.channel.send(embed);
     }
 }
