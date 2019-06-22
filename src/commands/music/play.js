407c95dbbe97e5bfd799a377f8ecd3ff476242d8
Thomas Achatz <30968528+shadeRed@users.noreply.github.com>
2019-06-22 09:18:19 -0500

    added playlist searching to play command

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -55,73 +55,113 @@ module.exports = {
             if (imports.guild.me.voiceChannel) {
                 if (imports.member.voiceChannel.id == imports.guild.me.voiceChannel.id) {
                     if (imports.member.voiceChannel.permissionsFor(imports.client.user).has('SPEAK')) {
-    
-                        try {
-                            var video = await imports.youtube.getVideo(parameters[0]);
-                            imports.channel.send(add(imports, video, embed));
-                        }
-    
-                        catch(error) {
+                        if (parameters[0].startsWith('playlist ')) {
+                            parameters[0] = parameters[0].split('playlist ')[1];
+                            var playlists = await imports.youtube.searchPlaylists(parameters[0], 10);
+                            var playlistsArray = new Array();
+                            for (p in playlists) {
+                                var title = Discord.Util.escapeMarkdown(playlists[p].title);
+                                playlistsArray.push(`**[${parseInt(p)+1}]** - ${title}`);
+                            }
+
+                            embed.setDescription(playlistsArray.join('\n'));
+                            embed.setFooter(`enter a number 1-${playlists.length}`);
+                            var selection = await imports.channel.send(embed);
+
                             try {
-                                var playlist = await imports.youtube.getPlaylist(parameters[0]);
+                                var response = await imports.channel.awaitMessages(message => message.content > 0 && message.content < playlists.length+1 && message.author.id == imports.user.id, {
+                                    maxMatches: 1,
+                                    time: 10000,
+                                    errors: ['time']
+                                });
+
+
+                                var playlist = playlists[parseInt(response.array()[0].content)-1];
                                 var videos = await playlist.getVideos();
                                 for (v in videos) { add(imports, videos[v], embed) }
-                                embed.setDescription(`${videos.length} tracks have been added to the queue`);
-                                imports.channel.send(embed);
+                                var successEmbed = new Discord.RichEmbed();
+                                successEmbed.setColor(imports.local.guild.colors.accent);
+                                successEmbed.setDescription(`${videos.length} tracks have been added to the queue`);
+                                selection.edit(successEmbed);
                             }
-    
+
+                            catch(error) {
+                                console.error(error);
+                                var errorEmbed = new Discord.RichEmbed();
+                                errorEmbed.setColor(imports.local.guild.colors.accent);
+                                errorEmbed.setDescription(`none or invalid value entered, canceling video selection`);
+                                selection.edit(errorEmbed);
+                            }
+                        }
+
+                        else {
+                            try {
+                                var video = await imports.youtube.getVideo(parameters[0]);
+                                imports.channel.send(add(imports, video, embed));
+                            }
+        
                             catch(error) {
                                 try {
-                                    var videos = await imports.youtube.searchVideos(parameters[0], 10);
-                                    var videosArray = new Array();
-                                    for (v in videos) {
-                                        var title = Discord.Util.escapeMarkdown(videos[v].title);
-                                        videosArray.push(`**[${parseInt(v)+1}]** - ${title}`);
-                                    }
-    
-                                    embed.setDescription(videosArray.join('\n'));
-                                    embed.setFooter(`enter a number 1-${videos.length}`);
-                                    var selection = await imports.channel.send(embed);
-    
-                                    try {
-                                        var response = await imports.channel.awaitMessages(message => message.content > 0 && message.content < videos.length+1 && message.author.id == imports.user.id, {
-                                            maxMatches: 1,
-                                            time: 10000,
-                                            errors: ['time']
-                                        });
-    
-                                        var video1 = videos[parseInt(response.array()[0].content)-1];
-                                        var video2 = await imports.youtube.getVideoByID(video1.id);
-    
-                                        var successEmbed = new Discord.RichEmbed();
-                                        successEmbed.setColor(imports.local.guild.colors.accent);
-                                        successEmbed = add(imports, video2, successEmbed);
-                                        selection.edit(successEmbed);
-                                    }
-    
-                                    catch(error) {
-                                        console.error(error);
-                                        var errorEmbed = new Discord.RichEmbed();
-                                        errorEmbed.setColor(imports.local.guild.colors.accent);
-                                        errorEmbed.setDescription(`none or invalid value entered, canceling video selection`);
-                                        selection.edit(errorEmbed);
-                                    }
-    
-    
-                                    /*
-                                    for (q in queue) {
-                                        var duration = `${queue[q].duration.minutes}:${queue[q].duration.seconds}`;
-                                        if (queue[q].duration.hours != 0) { duration = `${queue[q].duration.hours}:${duration}` }
-                                        var title = Discord.Util.escapeMarkdown(queue[q].title);
-    
-                                        if (q == 0) { queueText.push(`**${q+1}: ${title} - ${duration}**`) }
-                                        else { queueText.push(`${q+1}: ${title} - **${duration}**`) }
-                                    }*/
-                                }
-    
-                                catch(error) {
-                                    embed.setDescription(`no results found`);
+                                    var playlist = await imports.youtube.getPlaylist(parameters[0]);
+                                    var videos = await playlist.getVideos();
+                                    for (v in videos) { add(imports, videos[v], embed) }
+                                    embed.setDescription(`${videos.length} tracks have been added to the queue`);
                                     imports.channel.send(embed);
+                                }
+        
+                                catch(error) {
+                                    try {
+                                        var videos = await imports.youtube.searchVideos(parameters[0], 10);
+                                        var videosArray = new Array();
+                                        for (v in videos) {
+                                            var title = Discord.Util.escapeMarkdown(videos[v].title);
+                                            videosArray.push(`**[${parseInt(v)+1}]** - ${title}`);
+                                        }
+        
+                                        embed.setDescription(videosArray.join('\n'));
+                                        embed.setFooter(`enter a number 1-${videos.length}`);
+                                        var selection = await imports.channel.send(embed);
+        
+                                        try {
+                                            var response = await imports.channel.awaitMessages(message => message.content > 0 && message.content < videos.length+1 && message.author.id == imports.user.id, {
+                                                maxMatches: 1,
+                                                time: 10000,
+                                                errors: ['time']
+                                            });
+        
+                                            var video1 = videos[parseInt(response.array()[0].content)-1];
+                                            var video2 = await imports.youtube.getVideoByID(video1.id);
+        
+                                            var successEmbed = new Discord.RichEmbed();
+                                            successEmbed.setColor(imports.local.guild.colors.accent);
+                                            successEmbed = add(imports, video2, successEmbed);
+                                            selection.edit(successEmbed);
+                                        }
+        
+                                        catch(error) {
+                                            console.error(error);
+                                            var errorEmbed = new Discord.RichEmbed();
+                                            errorEmbed.setColor(imports.local.guild.colors.accent);
+                                            errorEmbed.setDescription(`none or invalid value entered, canceling video selection`);
+                                            selection.edit(errorEmbed);
+                                        }
+        
+        
+                                        /*
+                                        for (q in queue) {
+                                            var duration = `${queue[q].duration.minutes}:${queue[q].duration.seconds}`;
+                                            if (queue[q].duration.hours != 0) { duration = `${queue[q].duration.hours}:${duration}` }
+                                            var title = Discord.Util.escapeMarkdown(queue[q].title);
+        
+                                            if (q == 0) { queueText.push(`**${q+1}: ${title} - ${duration}**`) }
+                                            else { queueText.push(`${q+1}: ${title} - **${duration}**`) }
+                                        }*/
+                                    }
+        
+                                    catch(error) {
+                                        embed.setDescription(`no results found`);
+                                        imports.channel.send(embed);
+                                    }
                                 }
                             }
                         }
