9c2a9243d5f9f5d1f32c5b72bfd2155f42dc6439
Thomas Achatz <30968528+shadeRed@users.noreply.github.com>
2019-07-16 22:07:10 -0500

    recursive item loading

--- src/core/Items.js
+++ src/core/Items.js
@@ -3,11 +3,19 @@ var syncFs = require('./syncFs.js');
 var items = {};
 
 module.exports = async function() {
-    var files = await syncFs.readDir('./src/items/');
-    for (var f = 0; f < files.length; f++) {
-        var item = require(`./../items/${files[f]}`);
-        items[item.name] = item;
+    async function recur(path) {
+        var files = await syncFs.readDir(`./src/economy/${path}`);
+        for (var f = 0; f < files.length; f++) {
+            var isFolder = await syncFs.isFolder(`./src/economy/${path}/${files[f]}`);
+            if (isFolder) { await recur(`${path}/${files[f]}`) }
+            else {
+                var item = require(`./../economy/${path}/${files[f]}`);
+                items[item.name] = item;
+            }
+        }
     }
+
+    await recur(`items`);
 
     return items;
 }
